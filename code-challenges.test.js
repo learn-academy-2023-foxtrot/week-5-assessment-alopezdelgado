db5835ec39ed3bf0067a81126f145b0f114587b8
alopezdelgado <andrealdelgado_99@outlook.com>
2023-10-04 06:26:46 -0700

    Completed Challenges

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 5: JavaScript Coding Practical Questions with Jest
 
+// const { it } = require("node:test")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,45 +16,117 @@
 
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
+describe("codedFunc", () => {
+    it("takes in a string and returns a string with a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0.", () => {
+        const secretCodeWord1 = "Lackadaisical"
+        const secretCodeWord2 = "Gobbledygook"
+        const secretCodeWord3 = "Eccentric"
+        expect(codedFunc(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+        expect(codedFunc(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+        expect(codedFunc(secretCodeWord3)).toEqual("3cc3ntr1c")
+    })
+})
+
+// GOOD error:  ReferenceError: codedFunc is not defined
 
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+// name: codedFunc
+// input: string
+// output: string with certain characters changed. 
+// process: first, im going to create an object with the key as the letter to be changed and the value as the value that will be changed. Then, i am going to make an array of the string in order to map through the strings. I will create a const of codedCharacters and using map i will iterate through the characters in each string if the character (key) is in the object(using the in built in method), then if the character matches with one of the keys in the object will replace it with the value in the object. My catch all will be just returning the characters without changing them if they do not match with the key.I will want to convert everything to lower case during this process as well, in case one of the characters is upper case. Then i will return the characters, the unchanged and the changed ones together, use .join to convert them back into a string. 
+
+const codedFunc = (string) => { 
+    const codeLetters = {
+        'a': '4',
+        'e': '3',
+        'i': '1',
+        'o': '0' 
+    }
+    charactersArray = string.split('')
+    const codedCharacters = charactersArray.map(char => {
+        if (char.toLowerCase() in codeLetters){
+            return codeLetters[char.toLowerCase()]
+        } else {
+            return char
+        }
+    })
+    return codedCharacters.join('')
+}
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
 
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+describe("arrayLetter", () => {
+    it("takes in an array of words and a single letter and returns an array of all the words containing that particular letter.", () => {
+        const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+        const filterLetterA = "a"
+        const filterLetterE = "e"
+        expect(arrayLetter(fruitArray, filterLetterA)).toEqual(["Mango", "Apricot", "Peach"])
+        expect(arrayLetter(fruitArray, filterLetterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+    })
+})
 
-const filterLetterA = "a"
-// Expected output: ["Mango", "Apricot", "Peach"]
-const filterLetterE = "e"
-// Expected output: ["Cherry", "Blueberry", "Peach"]
-
+// GOOD ERROR: ReferenceError: arrayLetter is not defined
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+// name: arrayLetter
+// input: two arrays : fruitArray and filterLetterA or filterLetterE
+// output: depending on if filterLetterA or filterLetterE, an array that only includes the strings that have A or E in them 
+// process: create a function that takes in an array of words and depending on which letter is passed as a second input, return only the strings within the array that contain that letter. To do that, use filter method to filter through the strings, use .toLowerCase so that in case the letter is in Upper Case in the array, it can still filter through the word and return the word if it contains the letter (also turn to lowercase). If the string contains the letter, place it in a variable, then return the variable. 
+
+const arrayLetter = (word, letter) => {
+    const filteredWord = word.filter(word => word.toLowerCase().includes(letter.toLowerCase()))
+    return filteredWord
+}
+
+// PASSED
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
-// Expected output: false
-const hand4 = [7, 2, 7, 2, 7]
-// Expected output: true
+describe ("fullHouseFunc", () => {
+    it("takes in an array of 5 numbers and determines whether or not the array is a full house. A full house is exactly one pair and one three of a kind.", () => {
+        const hand1 = [5, 5, 5, 3, 3]
+        const hand2 = [5, 5, 3, 3, 4]
+        const hand3 = [5, 5, 5, 5, 4]
+        const hand4 = [7, 2, 7, 2, 7]
+        expect(fullHouseFunc(hand1)).toEqual(true)
+        expect(fullHouseFunc(hand2)).toEqual(false)
+        expect(fullHouseFunc(hand3)).toEqual(false)
+        expect(fullHouseFunc(hand4)).toEqual(true)
+    })
+})
+
+// GOOD failure: ReferenceError: fullHouseFunc is not defined
 
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+// name: fullHouseFunc
+// input: array of numbers (hand)
+// output: boolean value
+// process: first, im going to use let to assign hasPair and hasThreeOfAKind to false, to keep track of wether the hand has a pair and a three of a kind or not. Then, I will start a for loop that iterates through each number in the hand starting at index 0 and ending at the length of the hand, incrementing by one. Then, i will use a const to store the current number in the hand and name it num using hand[i]. To calculate how many times a unique number appears in the hand array i am going to use .filter and assign it a variable count, where it will check how many times the value of num is repeated in the hand, so at the end of my .filter function i am going to use .length and that will give me the count of occurences of num in the hand. this was the hard part, now for the easy part. I am going to create an if statement where if count is strictly equal to two, hasPair is going to be reassigned to true. AND another if statement to see if count ===3 then we return hasThreeOfAkind as true. Last if statement if hasPair and hasThreeOfAKind are strictly equal to true, then I return true. And, as the for loop return then I return hasPair and hasThreeOfAKind, and ifit is a fullhouse it should return true, and if it is not, it should return false. 
+
+const fullHouseFunc = (hand) => {
+    let hasPair = false
+    let hasThreeOfAKind = false
+    for(let i=0; i < hand.length; i++){
+        const num = hand [i]
+        const count = hand.filter(value => value === num).length 
+        if(count === 2){
+            hasPair = true
+        }
+        if(count === 3){
+            hasThreeOfAKind = true
+        }
+        if(hasPair && hasThreeOfAKind){
+            return true
+        }
+    }
+    return hasPair && hasThreeOfAKind
+}
--- interview-questions.md
+++ interview-questions.md
@@ -6,44 +6,68 @@ First, without external resources. Challenge yourself to answer from memory.
 
 Then, research the question to expand on your answer. Even if you feel you have answered the question completely on your own, there is always something more to learn. Write your researched answer in your OWN WORDS.
 
-Type your initials here to confirm you read the instructions:
+Type your initials here to confirm you read the instructions: DLD
 
 1. Why would super be used in a Ruby class?
 
-Your answer:
+Your answer: for inheritance, when you use super in a ruby class, you are inheriting values from a superclass. for example: i have my superclass Programming and my subclass Coding, in order for my Coding class to inherit the values from Programming, I am going to do this: 
 
-Researched answer:
+```rb 
+class Coding < Programming
+    super
+end
+```
+yay super, now i should inherit whatever values programming has into coding, yay coding 
 
+Researched answer: our lecture notes say that super is a method that is used to pass values of variables from the superclass to the subclass. And ruby guides says it can call on methods too! so you can inherit a method from the superclass to the subclass. For example :
+``` rb 
+class Videogame 
+     def name
+        p "Animal Crossing"
+     end
+end
+
+class Superior < Videogame
+    def name
+        super
+    end 
+end 
+
+superior_videogame = Superior.new 
+superior_videogame.name
+```
+
+AND this should print Animal Crossing! AMAZING
 2. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: it is a database similar to a spreadsheet, it has rows and columns, maybe its called relational, and im going on a stretch here, because the rows and columns have something in common, like each row is described by the columnn and the rows and columns NEED each other, they have a sort of relationship. I am sure there are other kinds of databases??? not sure. 
 
-Researched answer:
+Researched answer: no way i was right about the rows and columns being related to one another WOOHOO so oraxle says a relational database stores data and provides access to datapoints that are related to one another, this database is based on the relational model, which is a way of representing data in tables. The other kind of databases are non-relational. 
 
 3. What are primary keys? Why purpose does a primary key serve?
 
-Your answer:
+Your answer: A primary key is the unique key that one gives to each row in a relational database. It is a unique identifier for each of them. If I were to delete a value and add another one in its place and the pk's were in order, for example the deleted one's primary key was 6, the new one's will be 7, because it is unique for each entry to help organize. 
 
-Researched answer:
+Researched answer: Techopedia says that a primary key is designated in a relational database column to uniquely identify each table record, that way you can quickly parse (and i had to look up what parse is: it is to separate things into more easily processsed components) data within the table. So like, when we are extracting the data to do things with it or just to see it, the primary key makes it easier to identify each data entry since it is the unique key for each one. 
 
 4. What are the five HTTP verbs? What is each verb's corresponding CRUD action?
 
-Your answer:
+Your answer: get, post, put, patch and delete. Get's crud action is read, as it displays content. Post's crud action is create, as it makes new content. Put and patch's crud action is update because both put and patch modify content. And, delete's crud action is delete, because it removes content. 
 
-Researched answer:
+Researched answer: To add new content, you use crud create, which translates to the HTTP verb post. To display content you use crud read, which translates to get http verb. To modify content, you use crud update, which is http verb put and patch. And to remove content you delete, which is the same name for the http verb. 
 
 5. STRETCH: What is a JOIN table in SQL?
 
 Your answer:
 
-Researched answer:
+Researched answer: it is used to combine rows from tow or more tables based on a related column between them. 
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Model validations:
+1. Model validations: ruby on rails guides says it is the best way to ensure that only valid data is saved into your database. 
 
-2. Params:
+2. Params: params are parameters. its a way to access the user's data that they provide in your program 
 
-3. API:
+3. API: application programming interface. application is a software and interface is teh contract of service between two applicatios. This contract ishow the two communicate with each other using requests and responses. 
